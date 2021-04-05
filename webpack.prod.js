8419f07089c083c3bfdf234c5ed08f80312733af
EsraaNasr92 <esraanasr706@gmail.com>
2021-04-05 12:53:56 +0200

    minify css

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,11 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
@@ -9,7 +14,9 @@ module.exports = {
      libraryTarget: 'var',
      library: 'Client'
      },
-
+     optimization: {
+      minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+     },
     module: {
         rules: [
             {
@@ -18,15 +25,17 @@ module.exports = {
                 loader: "babel-loader"
             },
             {
-                test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-         }
+            test: /\.scss$/,
+            use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+
         ]
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new MiniCssExtractPlugin({ filename: "[name].css" })
     ]
 }
